b713ff9f359eaefee46039ff49fc0085fced67fc
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 19:08:38 +0200

    Limite filas a mostrar

--- usuarios/editar.php
+++ usuarios/editar.php
@@ -110,7 +110,7 @@
             }
             echo $totalREDES;
             for ($i=0; $i<$totalREDES; $i++){
-                $consultaRED = "SELECT * FROM `redes` ORDER BY `ID-RED` ASC";
+                $consultaRED = "SELECT * FROM `redes` ORDER BY `ID-RED` ASC LIMIT '.$i.', 1";
                 if ($resultadoRED = $conexion -> query($consultaRED)){
                     $redID = $resultadoRED->fetch_array(); 
                     echo '<div class="mdl-cell mdl-cell--4-col">
