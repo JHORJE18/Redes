d4b2db033f195b7418b34b8d2f08960db92b525a
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 17:23:15 +0200

    Ver ID de usuario

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
           <div class="demo-cards mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-grid mdl-grid--no-spacing">
             <div class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
               <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
-                <h2 class="mdl-card__title-text">Central Redes Sociales</h2>
+                <h2 class="mdl-card__title-text">Central Redes Sociales #<?php echo $_SESSION['ID']?></h2>
               </div>
               <div class="mdl-card__supporting-text mdl-color-text--grey-600">
                   En esta web puedes entrar a todas las redes sociales que tienen tus amigos.
