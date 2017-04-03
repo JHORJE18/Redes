a9d4b425576cc32e4ab5c76eb530c1a939fd6bb0
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 12:14:35 +0200

    Arreglar v1

--- plantilla/cabezera-body.php
+++ plantilla/cabezera-body.php
@@ -1,6 +1,3 @@
-<!--Google Analytics-->
-<?php include_once("analyticstracking.php") ?>
-
 <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
   <div class="mdl-layout__header-row">
     <span class="mdl-layout-title"><?php echo $seccion; ?></span>
