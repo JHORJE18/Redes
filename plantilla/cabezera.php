f3a2251f0910903b5e355a69c1189b1c12ff12b0
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 20:24:25 +0200

    Cambio colores

--- plantilla/cabezera.php
+++ plantilla/cabezera.php
@@ -20,6 +20,6 @@
 <link rel="shortcut icon" href="imagenes/favicon.png">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.lime-blue.min.css">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-blue.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <link rel="stylesheet" href="styles.css">
