cfcbe8199f42067ba60f932a27894e3e9aeb26eb
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 20:03:44 +0200

    Obtén link y nombre de red

--- perfil.php
+++ perfil.php
@@ -80,11 +80,15 @@
               if ($resultado = $conexion -> query($cuantasCNX)){
                 $numCNX = $resultado -> num_rows;
               }
-              echo 'consulta: '.$cuantasCNX.'<br>Num: '.$numCNX;
+
               if ($numCNX != null){
                 for ($i=0; $i<$numCNX; $i++){
                   //Muestra cada Red Social que tiene registrada
-                  $red = "SELECT * FROM conexion WHERE `ID-USUARIO`= '$perfilUser[0]' ORDER BY ASC LIMIT ".$i.", 1";
+                  $red = "SELECT conexion.`LINK-PERFIL`, redes.NOMBRE  FROM conexion INNER JOIN redes WHERE `ID-USUARIO`= '$perfilUser[0]' ORDER BY ASC LIMIT ".$i.", 1";
+                  if ($resultado = $conexion -> query($red)){
+                    $valores = $resultado ->fetch_array();
+                    echo '<a href="'.$valores[0].'" target="_blank"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">arrow_forward</i> '.$red[1].'</div></a>';
+                  }
                 }
               } else {
                 echo '<span>Este usuario aún no ha registrado ninguna Red Social :(</span>';
