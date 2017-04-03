b15df757eb8b56c5978dc491281018b5ff17f064
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 20:12:26 +0200

    Valor de la RED

--- perfil.php
+++ perfil.php
@@ -88,7 +88,7 @@
                   echo $red;
                   if ($resultado = $conexion -> query($red)){
                     $valores = $resultado -> fetch_array();
-                    echo '<a href="'.$valores[0].'" target="_blank"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">arrow_forward</i> '.$red[1].'</div></a>';
+                    echo '<a href="'.$valores[0].'" target="_blank"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">arrow_forward</i> '.$valores[1].'</div></a>';
                   }
                 }
               } else {
