acf15e4368569d7ccd1075d5400f0af336930d25
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-03 20:23:37 +0200

    Cambio diseño

--- plantilla/cabezera.php
+++ plantilla/cabezera.php
@@ -20,6 +20,6 @@
 <link rel="shortcut icon" href="imagenes/favicon.png">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.lime-blue.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <link rel="stylesheet" href="styles.css">
