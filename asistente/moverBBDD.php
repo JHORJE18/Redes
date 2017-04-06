b66c3941feb3a4bf2c9ea426a15ee59abdcfaca8
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:39:19 +0200

    Diseño

--- asistente/moverBBDD.php
+++ asistente/moverBBDD.php
@@ -3,7 +3,7 @@
     <center><img src="https://media.giphy.com/media/xTiTnxpQ3ghPiB2Hp6/giphy.gif"></center>
 </div>
 
-<div class="mdl-cell mdl-cell--4-col">
+<div class="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
     <span>¿Quieres mover tu Base de datos con todos los registros?</span>
     <a href="asistente.php?accion=moverBBDD?datos=SI"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">save</i>Mover con los registros</div></a>
     <a href="asistente.php?accion=moverBBDD?datos=NO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">skip_next</i>Mover sin los registros</div></a>
