c59800e194293b7d4c8d553b8ea6c2ea7792e073
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 12:02:30 +0200

    Descarga tabla.sql

--- asistente/moverBBDD.php
+++ asistente/moverBBDD.php
@@ -6,8 +6,11 @@
 <div class="mdl-cell mdl-cell--12-col">
     <span>El siguiente codigo es el encargado de generar la Base de Datos</span>
     <div class="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
-        <p>
-            <?php include 'asistente/sql.sql' ?>
-        </p>
+        
     </div>
+</div>
+
+<div class="mdl-cell mdl-cell--4-col">
+    <button class="mdl-cell mdl-cell--12-col mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Descargar codigo SQL</button>
+    <a href="asistente/tablas.sql" download="Tablas SQL"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">save</i> Descargar SQL</div></a>
 </div>
