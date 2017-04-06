f12d52b758d820dda6e7d175c19817870dc97540
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:38:02 +0200

    Mover BASE de DATOS

--- asistente.php
+++ asistente.php
@@ -2,9 +2,16 @@
 <html lang="es">
   <head>
     <?php include './plantilla/cabezera.php';
+
+    if ($_GET['accion'] != null){
+        $accion = $_GET['accion'];
+    }   else {
+        $accion = null;
+    }
+
     $seccion = "Asistente"?>
 
-    <title>Central Redes Sociales | Asistente</title>
+    <title>Central Redes Sociales | $seccion</title>
   </head>
   <body>
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
@@ -22,6 +29,30 @@
           <div class="mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-color--white">
             <div class="mdl-grid">
                 <?php 
+                switch ($accion){
+                    //Por defecto
+                    case null:
+                        include 'asistente/intro.php';
+                        break;
+                    default:
+                        include 'asistente/intro.php';
+                        break;
+
+                    //Instalación
+                    case "moverBBDD";
+                        include 'asistente/moverBBDD.php';
+                        break;
+
+                    //Añadir caracteristicas
+                    case "addRS";
+                        include 'asistente/addRS.php';
+                        break;
+                    
+                    //Eliminar
+                    case "delTODO":
+                        include 'asistente/delTODO.php';
+                        break;
+                }
                     include 'asistente/intro.php';
                 ?>              
             </div>
---
+++ asistente/moverBBDD.php
@@ -0,0 +1,10 @@
+<div class="mdl-cell mdl-cell--12-col">
+    <h1>Mover la Base de Datos</h1>
+    <center><img src="https://media.giphy.com/media/xTiTnxpQ3ghPiB2Hp6/giphy.gif"></center>
+</div>
+
+<div class="mdl-cell mdl-cell--4-col">
+    <span>¿Quieres mover tu Base de datos con todos los registros?</span>
+    <a href="asistente.php?accion=moverBBDD?datos=SI"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">save</i>Mover con los registros</div></a>
+    <a href="asistente.php?accion=moverBBDD?datos=NO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">skip_next</i>Mover sin los registros</div></a>
+</div>
