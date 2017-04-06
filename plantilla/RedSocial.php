7482c427ee96dfe790bd0a8b3c355109503e83f6
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 17:11:45 +0200

    Abre enlaces en nueva ventana

--- plantilla/RedSocial.php
+++ plantilla/RedSocial.php
@@ -1,16 +1,16 @@
 <div class="mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-color--white">
   <div class="mdl-grid">
     <div class="mdl-cell mdl-cell--12-col">
-        <a href="<?php echo $objeto[4] ?>"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">share</i>
+        <a href="<?php echo $objeto[4] ?>" target="_blank"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect">
         <?php echo $objeto[1]; ?>  </div></a>
         <hr>
     </div>
 
-    <div class="mdl-cell mdl-cell--2-col mdl-shadow--4dp" style="padding: 5px;">
+    <div class="mdl-cell mdl-cell--2-col" style="padding: 5px;">
       <img src="<?php echo $objeto[3] ?>"  style="width: 100%">
     </div>
 
-    <div class="mdl-cell mdl-cell--8-col mdl-shadow--4dp" style="padding: 5px;">
+    <div class="mdl-cell mdl-cell--8-col" style="padding: 5px;">
       <span><?php echo $objeto[2] ?></span>
     </div>
 
