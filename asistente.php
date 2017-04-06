7669835b4816adae29b056b334861f2c2de6764e
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:12:30 +0200

    Intro Asistente instalación

---
+++ asistente.php
@@ -0,0 +1,4 @@
+<?php
+
+include 'asistente/intro.php';
+?>
---
+++ asistente/intro.php
@@ -0,0 +1,3 @@
+<h1>Bienvenido al asistente</h1>
+<img src="https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif">
+<span>Seleccione lo que quieres hacer</span>
