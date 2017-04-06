22247f58b42e2ed5d26b6549eba4b2ca5af30bf0
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:21:14 +0200

    Acciones

--- asistente/intro.php
+++ asistente/intro.php
@@ -1,3 +1,20 @@
 <h1>Bienvenido al asistente</h1>
 <img src="https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif"><br>
-<span>Seleccione lo que quieres hacer</span>
+<div class="mdl-cell mdl-cell--4-col">
+    <span>Instalación</span>
+    <a href="asistente.php?accion=reinstalar"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">restore_page</i>Reinstalar la plataforma</div></a>
+    <a href="asistente.php?accion=moverBBDD"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">restore</i>Reinstalar la Base de datos</div></a>
+</div>
+
+<div class="mdl-cell mdl-cell--4-col">
+    <span>Añadir caracteristicas</span>
+    <a href="asistente.php?accion=add"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">add</i>Añadir redes sociales</div></a>
+</div>
+
+<div class="mdl-cell mdl-cell--4-col">
+    <span>Eliminar plataforma</span>
+    <a href="asistente.php?accion=del"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">delte</i>Eliminar TODOS los registros de la Base de datos</div></a>
+    <a href="asistente.php?accion=del"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">delte</i>Eliminar usuarios de la Base de datos</div></a>
+    <a href="asistente.php?accion=del"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">delte</i>Eliminar redes sociales registros Base de datos</div></a>
+    <a href="asistente.php?accion=del"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent"><i class="material-icons">delte</i>Eliminar conexiones de la Base de datos</div></a>
+</div>
