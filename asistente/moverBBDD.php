e57c516f681d60dcbd7b1941d6b32b7c968c4e8e
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:58:27 +0200

    SQL en texto plano

--- asistente/moverBBDD.php
+++ asistente/moverBBDD.php
@@ -3,12 +3,11 @@
     <center><img src="https://media.giphy.com/media/xTiTnxpQ3ghPiB2Hp6/giphy.gif"></center>
 </div>
 
-<div class="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
+<div class="mdl-cell mdl-cell--12-col">
     <span>El siguiente codigo es el encargado de generar la Base de Datos</span>
-    <form action="#">
-    <div class="mdl-textfield mdl-js-textfield">
-        <textarea class="mdl-textfield__input" type="text" rows= "10" id="sql" value="<?php include 'asistente/sql.txt'; ?>"></textarea>
-        <label class="mdl-textfield__label" for="sql">Codigo SQL</label>
+    <div class="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
+        <p>
+            <?php include 'asistente/sql.txt' ?>
+        </p>
     </div>
-    </form>
 </div>
