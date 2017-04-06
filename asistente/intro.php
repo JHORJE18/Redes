4ed70bd81450a8c854a3578484ae1d87c8309084
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:28:28 +0200

    Enlaces autocorregidos

--- asistente/intro.php
+++ asistente/intro.php
@@ -15,8 +15,9 @@
 
 <div class="mdl-cell mdl-cell--4-col">
     <button class="mdl-cell mdl-cell--12-col mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Eliminar</button>
-    <a href="asistente.php?accion=delTODO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar TODOS los registros de la Base de datos</div></a>
-    <a href="asistente.php?accion=delUSER"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar usuarios de la Base de datos</div></a>
-    <a href="asistente.php?accion=delREDES"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar redes sociales registros Base de datos</div></a>
-    <a href="asistente.php?accion=delCONX"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar conexiones de la Base de datos</div></a>
+    <a href="asistente.php?accion=delTODO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar TODO</div></a>
+    <a href="asistente.php?accion=delBBDD"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar Base de datos</div></a>
+    <a href="asistente.php?accion=delUSER"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar usuarios</div></a>
+    <a href="asistente.php?accion=delREDES"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar redes sociales</div></a>
+    <a href="asistente.php?accion=delCONX"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar conexiones</div></a>
 </div>
