1e985e24d667e131aee5a6826e5898979de9cf57
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 11:14:40 +0200

    Asistente con diseño

--- asistente.php
+++ asistente.php
@@ -1,4 +1,39 @@
-<?php
+<!doctype html>
+<html lang="es">
+  <head>
+    <?php include './plantilla/cabezera.php';
+    $seccion = "Asistente"?>
 
-include 'asistente/intro.php';
-?>
+    <title>Central Redes Sociales | Asistente</title>
+  </head>
+  <body>
+    <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+      <?php
+        include './plantilla/cabezera-body.php';
+        include './plantilla/menu-lateral.php';
+       ?>
+      <main class="mdl-layout__content mdl-color--grey-100">
+
+        <div class="mdl-grid demo-content">
+          <div class="mdl-cell mdl-cell--12-col mdl-shadow--4dp mdl-button mdl-button--raised mdl-button--colored"><center>
+            Asistente
+          </center></div>
+
+          <div class="mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-color--white">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--12-col"><center>
+                <?php 
+                    include 'asistente/intro.php';
+                ?>              
+              </div>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col mdl-shadow--4dp mdl-button mdl-button--raised mdl-button--colored"><center>
+            Asistente
+          </center></div>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
--- asistente/intro.php
+++ asistente/intro.php
@@ -1,3 +1,3 @@
 <h1>Bienvenido al asistente</h1>
-<img src="https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif">
+<img src="https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif"><br>
 <span>Seleccione lo que quieres hacer</span>
