7910c7583fd76885fdddb07bdf74fdc87b7cbe06
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-06 16:50:24 +0200

    Iconos corregidos

--- asistente/intro.php
+++ asistente/intro.php
@@ -14,9 +14,9 @@
 
 <div class="mdl-cell mdl-cell--4-col">
     <button class="mdl-cell mdl-cell--12-col mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Eliminar</button>
-    <a href="asistente.php?accion=delTODO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar TODO</div></a>
-    <a href="asistente.php?accion=delBBDD"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar Base de datos</div></a>
-    <a href="asistente.php?accion=delUSER"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar usuarios</div></a>
-    <a href="asistente.php?accion=delREDES"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar redes sociales</div></a>
-    <a href="asistente.php?accion=delCONX"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delte</i>Eliminar conexiones</div></a>
+    <a href="asistente.php?accion=delTODO"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delete_forever</i>Eliminar TODO</div></a>
+    <a href="asistente.php?accion=delBBDD"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delete</i>Eliminar Base de datos</div></a>
+    <a href="asistente.php?accion=delUSER"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delete</i>Eliminar usuarios</div></a>
+    <a href="asistente.php?accion=delREDES"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delete</i>Eliminar redes sociales</div></a>
+    <a href="asistente.php?accion=delCONX"><div class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect"><i class="material-icons">delete</i>Eliminar conexiones</div></a>
 </div>
