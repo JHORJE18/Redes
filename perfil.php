fecae713631003a47e1a97a1a18c1e48cf5c309f
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-09 02:52:19 +0200

    Muestra editar si es un perfil propio

--- perfil.php
+++ perfil.php
@@ -52,8 +52,7 @@
           </center></div>
 
           <?php
-            if ($usuarioVER = $_SESSION['ID']){
-              echo $usuarioVER.' '.$_SESSION['ID'];
+            if ($_GET['user'] = null){
             echo '<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
                     <a href="perfil.php?edit=1"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">build</i>Editar Perfil</div></a>
                   </div>';
