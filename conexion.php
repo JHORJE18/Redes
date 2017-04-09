482ed431c508d2fc5debf5c6f803a561487da8dd
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-09 03:27:00 +0200

    Conexión con comillas

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 // Database Constants
-$DB_SERVER = 'mysql.hostinger.es';
-$DB_USER = 'u553129575_jolog';
-$DB_PASS = 'jorgeguay';
-$DB_NAME = 'u553129575_redes';
+$DB_SERVER = "mysql.hostinger.es";
+$DB_USER = "u553129575_jolog";
+$DB_PASS = "jorgeguay";
+$DB_NAME = "u553129575_redes";
 
 $mysqli = new mysqli($DB_SERVER,$DB_USER, $DB_PASS,$BD_NAME);
 //$conexion = new mysqli("mysql.hostinger.es", "u553129575_jolog", "jorgeguay", "u553129575_redes");
