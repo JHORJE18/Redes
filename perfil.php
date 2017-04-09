59b5d28d2358a69eb3d6a6cbbc29a0958f37fd73
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-09 02:53:31 +0200

    Vuelta atras

--- perfil.php
+++ perfil.php
@@ -51,12 +51,9 @@
             Perfil: <?php echo $usuarioCOMPLETO ?>
           </center></div>
 
-          <?php
-            if ($_GET['user'] = null){
-            echo '<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
-                    <a href="perfil.php?edit=1"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">build</i>Editar Perfil</div></a>
-                  </div>';
-            } ?>
+          <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+            <a href="perfil.php?edit=1"><div class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">build</i>Editar Perfil</div></a>
+          </div>
 
           <div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--8-col">
             <div class="mdl-grid">
