4782823b64ae2761ede9fc02554a60153d3f3349
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-04-09 03:29:06 +0200

    Conexiones

--- conexion.php
+++ conexion.php
@@ -1,12 +1,4 @@
 <?php
-// Database Constants
-$DB_SERVER = "mysql.hostinger.es";
-$DB_USER = "u553129575_jolog";
-$DB_PASS = "jorgeguay";
-$DB_NAME = "u553129575_redes";
-
-$mysqli = new mysqli($DB_SERVER,$DB_USER, $DB_PASS,$BD_NAME);
-//$conexion = new mysqli("mysql.hostinger.es", "u553129575_jolog", "jorgeguay", "u553129575_redes");
-$conexion = new mysqli('$DB_SERVER','$DB_USER', '$DB_PASS','$BD_NAME');
-
+// Conexion con la Base de datos
+$conexion = new mysqli("mysql.hostinger.es", "u553129575_jolog", "jorgeguay", "u553129575_redes");
 ?>
